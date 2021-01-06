0a8a4f718e8835ea4d76866f756af121a0d06ba3
alireza <haji.encom@gmail.com>
2021-01-06 14:42:13 +0330

    initial commit

---
+++ app/src/main/java/com/alireza/simplemvvm/view/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.alireza.simplemvvm.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.alireza.simplemvvm.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
